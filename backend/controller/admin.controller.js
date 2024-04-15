da43f2c8f945d871c819be77a1ea2b7144a49a63
lakshyaA <lakshyaaggarwal2803@gmail.com>
2024-04-15 16:46:53 +0530

    added category of products

--- backend/controller/admin.controller.js
+++ backend/controller/admin.controller.js
@@ -1,4 +1,6 @@
 import { admin } from "../models/admin.models.js";
+import Category from "../models/category.models.js";
+import slugify from "slugify";
 
 export const addAdmin = async (req,res) => {
     try {
@@ -56,6 +58,103 @@ export const authAdmin = async (req,res) => {
                 id: loggedAdmin.adminId,
                 address: loggedAdmin.address
             }
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+//adding controllers for category
+// controller for creating category
+export const createCategory = async (req,res) => {
+    try {
+        const {name} = req.body;
+        if(!name){
+            return res.status(404).json({success:false,message:"Name is required"});
+        }
+        const existingCategory = await Category.findOne({name});
+        if(existingCategory){
+            return res.status(404).json({success:false,message:"Category already exists"});
+        }
+        const category = await new Category({name,slug:slugify(name)}).save();
+        res.status(200).json({
+            success:true,
+            message:"Category is added successfully",
+            category
+        });
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:"Something went wrong",
+            error: error.message
+        })
+    }
+}
+
+//controller for updating category
+export const updateCategory = async (req,res) => {
+    try {
+        const {name} = req.body;
+        const {id} = req.params;
+        const category = await Category.findByIdAndUpdate(id,{name,slug:slugify(name)},{new:true});
+        res.status(200).json({
+            success:true,
+            message: "Category updated successfully",
+            category
+        });
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+//get-all category controller
+export const getAllCategory = async (req,res) => {
+    try {
+        const allCategory = await Category.find({});
+        res.status(200).json({
+            success:true,
+            message:"All the categories",
+            allCategory
+        })
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message: error.message
+        })
+    }
+}
+
+// controller for getting single category
+export const singleCategory = async (req,res) => {
+    try {
+        const single = await Category.find({slug:req.params.slug});
+        res.status(200).json({
+            success: true,
+            message: "The category requested is fetched successfully",
+            single
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+// controller for deleting the category
+export const deleteCategory = async (req,res) => {
+    try {
+        const {id} = req.params;
+        await Category.findByIdAndDelete({id});
+        res.status(200).json({
+            success:true,
+            message: "category is deleted successfully"
         });
     } catch (error) {
         res.status(500).json({
--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -1,8 +1,20 @@
 import  express  from "express";
-import { addAdmin,authAdmin } from "../controller/admin.controller.js";
+import { 
+    addAdmin,authAdmin,
+    createCategory,deleteCategory,
+    getAllCategory,singleCategory, 
+    updateCategory 
+} from "../controller/admin.controller.js";
 const router = express.Router();
 
-router.route("/register").post(addAdmin);
+router.route("/register").post(addAdmin); 
 router.route("/login").post(authAdmin);
+
+//routes for category
+router.route("/create-category").post(createCategory);
+router.route("/update-category/:id").put(updateCategory);
+router.route("/getall-category").get(getAllCategory);
+router.route("/single-category/:id").get(singleCategory);
+router.route("/delete-category/:id").delete(deleteCategory)
 
 export default router;
